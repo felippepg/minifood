603b37db3678147f2a18512efdc060ff56b74b8e
Felippe Gonçalves <felippepg.goncalves@outlook.com.br>
2025-06-30 19:07:29 -0300

    feat: created preparing order method and added rabbitMQ service on docker

--- client-service/src/usecase/cart.ts
+++ client-service/src/usecase/cart.ts
@@ -2,9 +2,26 @@ import { StatusCodes } from 'http-status-codes';
 import logger from 'src/infra/logger';
 import { ICart } from 'src/interfaces/cart';
 import { ICartItem } from 'src/interfaces/cartItem';
+import { Order } from 'src/interfaces/order';
 import { createCartRepository, findCartByClientIdRepository, findCartByIdRepository } from 'src/repositories/cart.repository';
 import { createCartItemRepository } from 'src/repositories/cartItem.repository';
+import { sendOrder } from 'src/service/sendOrder.service';
 import { handlePrismaError } from 'src/utils/prisma-erro-handler';
+
+export type CartWithItems = {
+  id: number;
+  client_id: number;
+  created_at: Date;
+  updated_at: Date;
+  CartItem: {
+    id: number;
+    productName: string;
+    price: number;
+    quantity: number;
+    notes: string | null;
+    cart_id: number;
+  }[];
+};
 
 export const createCartUseCase = async (cart: ICart) => {
   try {
@@ -19,7 +36,7 @@ export const createCartUseCase = async (cart: ICart) => {
     }));
 
 
-    logger.info("adding itens to cart")
+    logger.info('adding itens to cart');
     for (const item of cartItens) {
       try {
         const created = await createCartItemRepository(item);
@@ -29,8 +46,15 @@ export const createCartUseCase = async (cart: ICart) => {
       }
     }
 
-    logger.info('return full object')
-    const fullCart = await findCartByIdRepository(newCart.id)
+    logger.info('return full object');
+    const fullCart = await findCartByIdRepository(newCart.id);
+
+    const order = prepareOrder(fullCart);
+
+    logger.info('calling send order');
+    await sendOrder(order);
+
+
     return {
       success: true,
       data: fullCart,
@@ -50,26 +74,45 @@ export const createCartUseCase = async (cart: ICart) => {
 
 export const findCartByClientIdUseCase = async (id: string) => {
   try {
-      const idNumber = Number(id);
+    const idNumber = Number(id);
     
-      if (isNaN(idNumber)) {
-        return {
-          success: false,
-          message: 'ID inválido',
-          code: 'CLIENTS_INVALID_ID',
-          statusCode: StatusCodes.BAD_REQUEST,
-        };
-      }
-      const cart = await findCartByClientIdRepository(idNumber)
+    if (isNaN(idNumber)) {
+      return {
+        success: false,
+        message: 'ID inválido',
+        code: 'CLIENTS_INVALID_ID',
+        statusCode: StatusCodes.BAD_REQUEST,
+      };
+    }
+    const cart = await findCartByClientIdRepository(idNumber);
 
-      if (!cart) {
-        return { success: true, data: null, statusCode: StatusCodes.NO_CONTENT };
-      }
+    if (!cart) {
+      return { success: true, data: null, statusCode: StatusCodes.NO_CONTENT };
+    }
 
-   return { success: true, data: cart, statusCode: StatusCodes.OK };
+    return { success: true, data: cart, statusCode: StatusCodes.OK };
   } catch (error) {
     logger.error('error while fetching cart by id', error);
     return handlePrismaError(error);
   }
-}
+};
+
+export const prepareOrder = (cart: CartWithItems | null): Order | undefined => {
+  if (!cart) {
+    logger.error('Cart is null, cannot transform into order');
+    return;
+  }
+
+  const orderItens = cart.CartItem.map((item) => ({
+    productName: item.productName,
+    quantity: item.quantity,
+    price: item.price,
+    notes: item.notes ?? undefined,
+  }));
+
+  return {
+    orderClientId: cart.client_id,
+    orderItens,
+  };
+};
 
